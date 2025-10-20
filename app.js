45f18380f0fe6699a1775fa47513b983d5eb545b
MeshachB <MeshachCBrown@gmail.com>
2025-10-20 09:33:46 -0500

    Add basic calculator logic for display, clear, and equals buttons

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ const display = document.querySelector('.display');
 buttons.forEach((button)=> {
     button.addEventListener('click' , (e) => {
         console.log(e.target.innerText); 
+        const value = e.target.innerText;
+       
+        if (value === 'C'){
+            display.innerText = '';
+        }   
+        else if (value === '=') {
+        display.innerText = eval(display.innerText);    
+        }
+        else{
         display.innerText += value;
+        }
+        
     });
 });
 /*-------------------------------- Functions --------------------------------*/
